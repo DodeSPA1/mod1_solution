73db7b28a09761f0a7300ea4f56af8ff9105403b
Jorge Serrada <unjorgete@msn.com>
2023-12-08 21:45:59 +0100

    wip(functionality): Set up functions and bounding for Angular JS

--- assignment1-starter-code/app.js
+++ assignment1-starter-code/app.js
@@ -1,3 +1,24 @@
 (function(){
-    angular.module('LunchCheck',[]);
+    'use strict'; // Protect us from crete global scope variables 
+    angular.module('LunchCheck',[])
+
+    .controller('LunchCheckController', function ($scope){
+        $scope.food = '';
+
+        $scope.diplayResult = function (){
+            var totalFood = checkHowmuchFood($scope.food);
+            $scope.reult = totalFood;
+        };
+
+        function checkHowmuchFood(str){
+            const totalDishes = str.split(",")
+            if (totalDishes >= 3){
+                return "Enjoy!"
+            }else{
+                return "Too much!"
+            } 
+
+        }
+
+    });
 }) ();
